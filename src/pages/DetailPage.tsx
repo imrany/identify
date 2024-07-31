0b86cab2b25810ca26ed8b4e3ad237722a805386
imrany <imranmat254@gmail.com>
2024-07-31 19:29:08 +0300

    Details page ui update

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,13 +1,34 @@
 import { GlobalContext } from "../context";
 import { useContext } from "react";
+import Cactus from "../assets/cactus.png"; 
+import { PiStudentBold } from "react-icons/pi";
+import { useNavigate } from "react-router-dom";
 
 export default function DetailPage(){
     const { details }=useContext(GlobalContext);
+    const navigate=useNavigate()
     console.log(JSON.stringify(details))
     return(
-        <div className="text-[var(--primary-02)] bg-[var(--primary-01)] h-screen">
-            <p>Detail page</p>
-            <p>{details.fullName}</p>
+        <div className="text-[var(--primary-02)] flex flex-col items-center pb-8 bg-[var(--primary-01)] h-screen">
+            <div className="w-screen bg-[var(--button-bg-01)] text-[var(--primary-01)] h-[50px] flex items-center justify-center">
+                <p>Access granted</p>
+            </div>
+            <div className="flex justify-center gap-4 mt-6 h-[120px]">
+                <img src={Cactus} className="rounded-[20px] object-fit h-[120px] w-[120px]" alt="Student's image"/>
+                <div className="flex-grow flex flex-col gap-3 h-full">
+                    <div>
+                        <p className="text-xl font-semibold max-w-[200px]">Imran Matano Ngati</p>
+                        <p className="text-sm text-[var(--primary-03)]">Bachelor of computer science</p>
+                        <p className="text-sm text-[var(--primary-03)]">CSC/034/2021</p>
+                        <p className="text-sm text-[var(--primary-03)]">SSAE</p>
+                    </div>
+                    <div className="text-base text-[var(--button-bg-01)] items-center flex gap-2">
+                        <PiStudentBold className=""/>
+                        <p>student</p>
+                    </div>
+                </div>
+            </div>
+            <button className="flex mt-auto justify-center items-center rounded-[30px] w-[90vw] text-[var(--primary-01)] h-[43px] bg-[var(--button-bg-01)]" onClick={()=>navigate(-1)}>Next</button>
         </div>
     )
 }
