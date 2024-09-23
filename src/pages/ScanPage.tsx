7a0ba2129aeae105eabdfe8e704073a31b841959
imrany <imranmat254@gmail.com>
2024-09-23 16:03:30 +0300

    trying to access device camera

--- src/pages/ScanPage.tsx
+++ src/pages/ScanPage.tsx
@@ -1,10 +1,42 @@
 import { Scanner } from '@yudiel/react-qr-scanner';
 import { GlobalContext } from "../context";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function ScanPage(){
     const { authenticate, showErrorDialog }=useContext(GlobalContext);
+    const [isVideo,SetIsVideo]=useState(true)
+
+    async function startRecording(){
+        if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+            // ok, browser supports it
+            try{
+                const constraints = {
+                    video: {
+                        width: {
+                            min: 1280,
+                            ideal: 1920,
+                            max: 2560,
+                        },
+                        height: {
+                            min: 720,
+                            ideal: 1080,
+                            max: 1440,
+                        },
+                        //facingMode: "environment"
+                    },
+                }
+                const video = document.querySelector('#video')
+                const videoStream = await navigator.mediaDevices.getUserMedia(constraints)
+                video.srcObject = videoStream
+            }catch(error:any){
+                console.log(error)
+                //showErrorDialog("Error",error.message)
+                SetIsVideo(false)
+            }
+        }
+    }
+
     const navigate=useNavigate()
     function handleScan(result:any){
         result.forEach((i:any)=>{
@@ -29,14 +61,31 @@ export default function ScanPage(){
             }
         })
     }
+
+    useEffect(()=>{
+        startRecording()
+    },[])
     return(
-        <div className="flex flex-col justify-center items-center max-h-screen bg-[var(--primary-01)]">
-            <Scanner
-                allowMultiple={true}
-                classNames={{container:"my-[23vh]"}} 
-                onScan={(result) => handleScan(result)}
-                scanDelay={1500}
-            />
-        </div>
+        <>
+        {isVideo?(
+            <video autoPlay id="video" className="flex flex-col justify-center items-center max-h-screen">
+                <Scanner
+                    allowMultiple={true}
+                    classNames={{container:"my-[23vh]"}} 
+                    onScan={(result) => handleScan(result)}
+                    scanDelay={1500}
+                />
+            </video>
+        ):(
+            <div className="flex flex-col justify-center items-center max-h-screen bg-[var(--primary-01)]">
+                <Scanner
+                    allowMultiple={true}
+                    classNames={{container:"my-[23vh]"}} 
+                    onScan={(result) => handleScan(result)}
+                    scanDelay={1500}
+                />
+            </div>
+        )}
+        </>
     )
 }
